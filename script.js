6ec2f8a8f185d6b88e38d8bcdc3b360f751a4007
juanmfretes <juanmfretes@gmail.com>
2022-04-10 21:23:24 -0400

    finish thumbnail preview in slider mode

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+/*
+FALTA:
+1. Añadir data-num="número" a cada imagen "thumbnail"
+2. Crear un nodelist con la clase "slider-item"
+3. Ubicar los slider-items uno al lado del otro con transform()
+4. Hacer que cuando se haga click en la imagen se muestre la imagen "grande" y abajo una especie de miniaturas de cada imagen (modificando el "gallery" container)
+*/
