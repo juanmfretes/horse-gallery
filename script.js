fed8a4619e2233ccf59f3a9f9bf6f7674452a470
juanmfretes <juanmfretes@gmail.com>
2022-04-18 15:34:54 -0400

    changes: * code refactor * add slider mode control with arrow keys * add 2 new pictures * save vertical scroll when entering slider mode

--- script.js
+++ script.js
@@ -10,16 +10,17 @@ FALTA:
 7. Ver cómo usar gestos
 [YA] 8. Hacer que cuando se haga click en la imagen, se muestre la imagen "grande" y abajo una especie de miniaturas de cada imagen (Slider Mode)
 [YA] 9. Hacer que al presionar "Escape" cuando se está en modo SLIDE vuelva  al modo grilla
-10. Hacer un "Code Refactor" (Hay mucho código repetido hasta ahora)
-11. Hacer que se mantenga el scroll al salir del "Slider MODE"
+[YA] 10. Hacer un "Code Refactor" (Hay mucho código repetido hasta ahora)
+[ULTIMO Y YA PODÉS PUBLICAR]11. Hacer que se mantenga el scroll al salir del "Slider MODE"
 12. Al hacer click en una miniatura, que se haga un scroll a las fotografías y lleve automáticamente hasta esa imagen en grande.
 13. Ver si podés implementar un scroll infinito (moviendo los 10 primeros elementos del SliderItems y del gallery Items al final de la lista cada vez que se está cerca del final de la lista)
 14. Poner una especie de "backdrop-filter" al frente de todo durante 5 segundos antes de mostrar la página. Usar el DOMContentLoaded Event. Hacer esto para que de tiempo de cargar un poco la página. Ver qué mensaje podés mostrar
 [YA] 15. Crear un <button> que sirva para ocultar o mostrar las miniaturas (si se oculta, imagen ocupa la pantalla completa)
 16. Añadir zoom a las imagenes
 [YA] 17. Añadir boton para cerrar el "slider mode"
-18. Hacer que se maneje con teclas (flechas para pasar imagenes, escape para salir, abajo y arriba para ocultar/mostrar barra de miniaturas)
+[YA] 18. Hacer que se maneje con teclas (flechas para pasar imagenes, escape para salir, abajo y arriba para ocultar/mostrar barra de miniaturas)
 19. Aplicarle un normalizador css
+20. Responsive design
 
 */
 
@@ -50,25 +51,22 @@ let currentMin = 0; //guarda la miniatura del slide actual
 const limitLeftIndex = 2; // máximo translate (izq) de las miniaturas
 const limitRightIndex = galleryItems.length - 3; // máximo translate (der) de las miniaturas
 let minOffset = 0; // para centrar la miniatura actual
-let hiddenMiniatureBar = false; // bandera para saber si se cerró o no la barra de miniaturas
+let hiddenMiniatureBar = false; // bandera para saber si se ocultó (o no) la barra de miniaturas
+
+// Gestionar scroll
+let scrollY; // guarda el ScrollYOffset
 
 /* FUNCTIONS */
-// PONER EN "SLIDE" MODE (CREO QUE PODÉS BORRAR ESTAS DOS FUNCIONES. ANALZAR TODO EL CÓDIGO PRIMERO PARA VER SI ES ASÍ)
-const displaySlides = function () {
-  sliderItems.forEach((slide, i) => {
-    slide.style.setProperty("transform", `translateX(${i * spacerSl}px)`);
-  });
+// SHOW SLIDER
+const showSlider = function (el) {
+  sliderBox.classList.remove("hidden");
+  el.classList.add("active");
+  galleryItems.forEach((miniature) => miniature.classList.add("miniature"));
+  gallery.classList.add("spread");
 };
 
-const displayMiniatures = function () {
-  galleryItems.forEach((miniature, i) => {
-    miniature.style.setProperty("transform", `translateX(${i * spacerMin}px)`);
-  });
-};
-
-/* EVENT LISTENERS */
-// Poner en "Grid Mode" al cargar la página
-window.addEventListener("DOMContentLoaded", function () {
+// HIDE SLIDER
+const hideSlider = function () {
   sliderBox.classList.add("hidden");
   galleryItems.forEach((miniature) =>
     miniature.classList.remove("miniature", "active")
@@ -77,184 +75,197 @@ window.addEventListener("DOMContentLoaded", function () {
     miniature.style.setProperty("transform", "none")
   );
   gallery.classList.remove("spread");
-});
+};
 
-// Entrar al "Slider Mode"
-gallery.addEventListener("click", function (event) {
-  const element = event.target.closest(".gallery-item");
-  if (!element) return;
-  else if (element.classList.contains("miniature")) {
-    console.log("miniature");
-    return;
-  }
-  currentPic = currentMin =
-    event.target.closest(".gallery-item").dataset.num - 1;
-  sliderBox.classList.remove("hidden");
-  event.target.closest(".gallery-item").classList.add("active");
-  galleryItems.forEach((miniature) => miniature.classList.add("miniature"));
-  gallery.classList.add("spread");
-  // displayMiniatures();
-  // displaySlides();
-  sliderItems.forEach((slide, i) => {
-    slide.style.setProperty(
-      "transform",
-      `translateX(${(i - currentPic) * spacerSl}px)`
-    );
-  });
-  if (currentPic < 2) minOffset = 0;
-  else if (currentPic > limitRightIndex - 2) minOffset = limitRightIndex - 2;
-  else minOffset = currentPic - 2;
+// Show MINIATURE BAR or GALLERY
+const showGalleryOrMiniatureBar = function () {
+  gallery.classList.remove("hidden");
+};
+
+// Hide MINIATURE BAR or GALLERY
+const HideGalleryOrMiniatureBar = function () {
+  gallery.classList.add("hidden");
+};
+
+// HIDE/SHOW MINIAUTURES FUNCTIONALITY
+const miniatureHideShow = function (hidden) {
+  // if hidden => show bar; if not hidden => hide bar
+  if (hidden) showGalleryOrMiniatureBar();
+  else HideGalleryOrMiniatureBar();
+  // change direction of btn arrow
+  btnHideShow.textContent = hidden ? "↓" : "↑";
+  // slider fullscreen
+  sliderBox.classList.toggle("fullHeight", !hidden);
+  // update flag state
+  hiddenMiniatureBar = !hiddenMiniatureBar;
+};
+
+// CHANGE MINIATURE
+const changeMiniature = function () {
   galleryItems.forEach((miniature, i) => {
     miniature.style.setProperty(
       "transform",
       `translateX(${(i - minOffset) * spacerMin}px)`
     );
   });
+};
 
-  // Ocultar la barra de miniaturas si es que se ocultó anteriormente
-  if (hiddenMiniatureBar) {
-    // Ocultar barra de miniaturas
-    gallery.classList.add("hidden");
-  }
-});
+// ACTIVATE MINIATURE
+const activateMiniature = function () {
+  galleryItems.forEach((miniature) => miniature.classList.remove("active"));
+  galleryItems[currentPic].classList.add("active");
+};
 
-// Cerrar "Slider mode" (Método 1: Hacer click en el botón "X")
-btnClose.addEventListener("click", function () {
-  sliderBox.classList.add("hidden");
-  galleryItems.forEach((miniature) =>
-    miniature.classList.remove("miniature", "active")
-  );
-  galleryItems.forEach((miniature) =>
-    miniature.style.setProperty("transform", "none")
-  );
-  gallery.classList.remove("spread");
-  // Por si la barra de miniaturas esté oculta
-  gallery.classList.remove("hidden");
-});
-
-// Cerrar "Slider mode" (Método 2: Presionar "Escape")
-document.addEventListener("keydown", function (event) {
-  console.log("captó evento");
-  if (event.key === "Escape" && gallery.classList.contains("spread")) {
-    console.log("entró");
-    sliderBox.classList.add("hidden");
-    galleryItems.forEach((miniature) =>
-      miniature.classList.remove("miniature", "active")
-    );
-    galleryItems.forEach((miniature) =>
-      miniature.style.setProperty("transform", "none")
-    );
-    gallery.classList.remove("spread");
-    // Por si la barra de miniaturas esté oculta
-    gallery.classList.remove("hidden");
-  }
-});
-
-// Ocultar la barra de miniaturas
-btnHideShow.addEventListener("click", function () {
-  // 1. Si estaba oculta, 2. Si estaba visible
-  if (hiddenMiniatureBar) {
-    // Desplegar barra de miniaturas
-    gallery.classList.remove("hidden");
-    // Cambiar direccion de la flecha del btn
-    btnHideShow.textContent = "↓";
-    // Quitar el "fullscreen mode"
-    sliderBox.classList.remove("fullHeight");
-    // Modificar bandera
-    hiddenMiniatureBar = !hiddenMiniatureBar;
-  } else {
-    // Ocultar barra de miniaturas
-    gallery.classList.add("hidden");
-    // Cambiar direccion de la flecha del btn
-    btnHideShow.textContent = "↑";
-    // Agregar el "fullscreen mode"
-    sliderBox.classList.add("fullHeight");
-    // Modificar bandera
-    hiddenMiniatureBar = !hiddenMiniatureBar;
-  }
-
-  console.log(hiddenMiniatureBar);
-});
-
-// RIGHT SLIDER BUTTON
-btnRight.addEventListener("click", function () {
-  console.log(minOffset);
-  if (currentPic < maxPic) currentPic++;
-  else {
-    currentPic = 0;
-    minOffset = 0;
-
-    // Translate miniatures (repetido) [Sólo para pasar de extremo a otro]
-    galleryItems.forEach((miniature, i) => {
-      miniature.style.setProperty(
-        "transform",
-        `translateX(${(i - minOffset) * spacerMin}px)`
-      );
-    });
-  }
-
-  // Modify Slides
+// CHANGE SLIDE
+const changeSlide = function () {
   sliderItems.forEach((slide, i) => {
     slide.style.setProperty(
       "transform",
       `translateX(${(i - currentPic) * spacerSl}px)`
     );
   });
+};
 
-  // Translate Miniatures (Pasa a la miniatura de al lado)
-  if (currentPic > limitLeftIndex && currentPic <= limitRightIndex) {
-    minOffset++;
-    console.log(minOffset);
-    galleryItems.forEach((miniature, i) => {
-      miniature.style.setProperty(
-        "transform",
-        `translateX(${(i - minOffset) * spacerMin}px)`
-      );
-    });
-  }
+// CLOSE SLIDER MODE
+const closeSliderMode = function () {
+  hideSlider();
+  // Por si la barra de miniaturas esté oculta
+  showGalleryOrMiniatureBar();
+  // Mantener scroll vertical previo a entrar al Slider Moder
+  window.scrollTo({
+    top: scrollY,
+  });
+};
 
-  // Modify Miniatures
-  galleryItems.forEach((miniature) => miniature.classList.remove("active"));
-  galleryItems[currentPic].classList.add("active");
-});
+// UPDATE VIEW
+const updateView = function () {
+  // Change Slide
+  changeSlide();
 
-// LEFT SLIDER BUTTON
-btnLeft.addEventListener("click", function () {
+  // Change miniature
+  changeMiniature();
+
+  // Activate current miniature
+  activateMiniature();
+};
+
+// PREVIOUS SLIDE
+const previousSlide = function () {
   if (currentPic > 0) currentPic--;
   else {
     currentPic = maxPic;
     minOffset = maxPic - 4;
-
-    // Translate miniatures (repetido) [Sólo para pasar de extremo a otro]
-    galleryItems.forEach((miniature, i) => {
-      miniature.style.setProperty(
-        "transform",
-        `translateX(${(i - minOffset) * spacerMin}px)`
-      );
-    });
   }
 
-  // Modify Slides
-  sliderItems.forEach((slide, i) => {
-    slide.style.setProperty(
-      "transform",
-      `translateX(${(i - currentPic) * spacerSl}px)`
-    );
-  });
-
-  // Translate Miniatures (Pasa a la miniatura de al lado)
+  // Desplazar miniaturas hacia la derecha a medida que se pasan slides
   if (currentPic >= limitLeftIndex && currentPic < limitRightIndex) {
     minOffset--;
-    console.log(minOffset);
-    galleryItems.forEach((miniature, i) => {
-      miniature.style.setProperty(
-        "transform",
-        `translateX(${(i - minOffset) * spacerMin}px)`
-      );
-    });
   }
 
-  // Modify Miniatures
-  galleryItems.forEach((miniature) => miniature.classList.remove("active"));
-  galleryItems[currentPic].classList.add("active");
+  updateView();
+};
+
+// NEXT SLIDE
+const nextSlide = function () {
+  if (currentPic < maxPic) currentPic++;
+  else {
+    currentPic = 0;
+    minOffset = 0;
+  }
+
+  // Desplazar miniaturas hacia la izquierda a medida que se pasan slides
+  if (currentPic > limitLeftIndex && currentPic <= limitRightIndex) {
+    minOffset++;
+  }
+
+  updateView();
+};
+
+// SAVE SCROLL DISTANCE
+const saveScrollDistance = function () {
+  scrollY = window.scrollY;
+};
+
+/* EVENT LISTENERS */
+// Poner en "Grid Mode" al cargar la página
+// OBS: para que cargue más rápido, se puede modificar el HTLM para que inicie en GRID MODE y no haga falta esta parte!!!!
+window.addEventListener("DOMContentLoaded", function () {
+  hideSlider();
+  console.log(hiddenMiniatureBar);
+});
+
+// ABRIR "Slider Mode"
+gallery.addEventListener("click", function (event) {
+  const element = event.target.closest(".gallery-item");
+  // Casos especiales
+  if (!element) return;
+  else if (element.classList.contains("miniature")) return;
+
+  // Save Scroll Distance
+  saveScrollDistance();
+  console.log(scrollY);
+
+  currentPic = currentMin = element.dataset.num - 1;
+  showSlider(element);
+  changeSlide();
+  if (currentPic < 2) minOffset = 0;
+  else if (currentPic > limitRightIndex - 2) minOffset = limitRightIndex - 2;
+  else minOffset = currentPic - 2;
+  changeMiniature();
+
+  // Ocultar la barra de miniaturas si es que se ocultó anteriormente
+  if (hiddenMiniatureBar) {
+    HideGalleryOrMiniatureBar();
+  }
+});
+
+// CERRAR "Slider mode" (Método 1: Hacer click en el botón "X")
+btnClose.addEventListener("click", function () {
+  closeSliderMode();
+});
+
+// Cerrar "Slider mode" (Método 2: Presionar "Escape")
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape" && gallery.classList.contains("spread")) {
+    closeSliderMode();
+  }
+});
+
+// Ocultar/Mostrar la barra de miniaturas (Método 1: Usando btn "↑"/"↓")
+btnHideShow.addEventListener("click", function () {
+  miniatureHideShow(hiddenMiniatureBar);
+});
+
+// Ocultar/Mostrar la barra de miniaturas (Método 2: Usando flechas del teclado)
+document.addEventListener("keydown", function (event) {
+  if (sliderBox.classList.contains("hidden")) return;
+  else if (
+    (event.key === "ArrowUp" && hiddenMiniatureBar) ||
+    (event.key === "ArrowDown" && !hiddenMiniatureBar)
+  )
+    miniatureHideShow(hiddenMiniatureBar);
+});
+
+// RIGHT SLIDER BUTTON (Método 1: Usando btn "➡")
+btnRight.addEventListener("click", function () {
+  nextSlide();
+});
+
+// RIGHT SLIDER BUTTON (Método 2: Usando la tecla "➡")
+document.addEventListener("keydown", function (event) {
+  console.log(event.key);
+  if (sliderBox.classList.contains("hidden")) return;
+  if (event.key === "ArrowRight") nextSlide();
+});
+
+// LEFT SLIDER BUTTON (Método 1: Usando btn "⬅")
+btnLeft.addEventListener("click", function () {
+  previousSlide();
+});
+
+// LEFT SLIDER BUTTON (Método 2: Usando la tecla "⬅")
+document.addEventListener("keydown", function (event) {
+  console.log(event.key);
+  if (sliderBox.classList.contains("hidden")) return;
+  if (event.key === "ArrowLeft") previousSlide();
 });
